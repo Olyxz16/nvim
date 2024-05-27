1833c6563d0bba0eb2cc23879613434b1ba3c596
Cédric Sazos <cedric.sazos@tutanota.com>
2024-05-27 18:51:12 +0200

    added trouble shortcut

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,4 +1,8 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+
+    vim.keymap.set("n", "<leader>tt", function()
+        require("trouble").toggle()
+    end);
 }
