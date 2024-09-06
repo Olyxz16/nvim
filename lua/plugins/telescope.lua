1da5e06447137c2f9c57b518cf1c463d7d61d779
Cédric Sazos <cedric.sazos@tutanota.com>
2024-09-06 15:33:36 +0200

    changed file finder keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader><Tab>", builtin.find_files, {})
       vim.keymap.set("n", "<leader><leader>", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
       vim.keymap.set("n", "<leader>p", ":Telescope neovim-project discover<CR>", {})
