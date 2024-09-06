7873ded3476129e3ecd5530498df6801ae808c78
Cédric Sazos <cedric.sazos@tutanota.com>
2024-09-06 15:33:47 +0200

    make mason lazy load

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,12 @@
 return {
     {
         "williamboman/mason.nvim",
-        --lazy = false,
         config = function()
             require("mason").setup()
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        --lazy = false,
         config = function()
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             require("mason-lspconfig").setup({
@@ -43,7 +41,6 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        --lazy = false,
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities;
