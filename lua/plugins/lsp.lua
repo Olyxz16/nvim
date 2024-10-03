7c534f91cb03e7daaffb65e0c9ddd8fb579bc097
Cédric Sazos <cedric.sazos@tutanota.com>
2024-10-04 00:24:06 +0200

    Rewrote the whole lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,69 @@
 return {
-    {
+    "neovim/nvim-lspconfig",
+    dependencies = {
         "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            require("mason-lspconfig").setup{}
-        end,
-        opts = {
-            auto_install = true,
-        },
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/nvim-cmp"
     },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities();
-            -- C setup
-            lspconfig.clangd.setup({cmd={'clangd'}})
-            -- Templ setup
-            lspconfig.tailwindcss.setup({
-                capabilities = capabilities,
-                filetypes = { "css", "templ", "astro", "javascript", "typescript", "react", "html" },
-                init_options = { userLanguages = { templ = "html" } }
-            })
-            lspconfig.cssls.setup({
-                settings = { css = { validate = true, lint = { unknownAtRules = "ignore", }, }, },
-            })
-            local templConf = { capabilities = capabilities, filetypes = { "html", "templ" } }
-            lspconfig.html.setup(templConf)
-            lspconfig.htmx.setup(templConf)
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
+    ensureInstalled = {
+        "gopls",
     },
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.prettier,
-                },
-            })
 
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        end,
-    }
+    config = function()
+        local cmd = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+        local lspconfig = require("lspconfig")
+        local templConf = { capabilities = capabilities, filetypes = { "html", "templ" } }
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = ensureInstalled,
+            handlers = {
+                function(server_name)
+                    lspconfig[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                ["clangd"] = function() 
+                    lspconfig.clangd.setup {
+                        capabilities = capabilities,
+                        cmd={ 'clangd' },
+                    }
+                end,
+                ["cssls"] = function()
+                    lspconfig.cssls.setup {
+                        capabilities = capabilities,
+                        settings = { css = { valid = true, 
+                                             lint = { unknowAtRules = "ignore", }, }, },
+                    } 
+                end,
+                ["tailwindcss"] = function()
+                    lspconfig.tailwindcss.setup {
+                        capabilities = capabilities,
+                        filetype = { "css", "templ", "astro", "javascript", "typescript", "html" },
+                        init_options = { userLanguages = { templ = { "html" } } },
+                    }
+                end,
+                ["html"] = function()
+                    lspconfig.html.setup(templConf)
+                end,
+                ["htmx"] = function()
+                    lspconfig.htmx.setup(templConf)
+                end,
+            },
+        })
+    end,
 }
