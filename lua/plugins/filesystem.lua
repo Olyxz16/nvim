2a216562e57aa01e13331982f7e3a098e7c0f628
Cédric Sazos <cedric.sazos@tutanota.com>
2024-10-03 12:19:52 +0200

    Added harpoon

--- lua/plugins/filesystem.lua
+++ lua/plugins/filesystem.lua
@@ -24,12 +24,22 @@ return {
             },
         },
     },
-    {
+    --[[{
         "stevearc/oil.nvim",
         config = function()
             local oil = require("oil")
             oil.setup()
             vim.keymap.set("n", "-", oil.toggle_float, {})
         end,
+    },]]
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        vim.keymap.set('n', '<leader><Tab>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>'),
+        vim.api.nvim_create_user_command('Mark', function ()
+            require("harpoon.mark").add_file()
+        end, {})
     }
 }
