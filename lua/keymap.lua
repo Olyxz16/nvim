d28daa32bd384892a80bc40aa7683de60b3089e1
Cédric Sazos <cedric.sazos@tutanota.com>
2024-12-12 16:09:06 +0100

    fix: set esc nohl keymap silent

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,12 +1,11 @@
 vim.keymap.set('n', '<leader>e', ':Neotree toggle position=right<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>t', ':belowright split<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
-vim.keymap.set('n', '<esc>', ':nohlsearch<CR>')
 
 vim.keymap.set('n', '<Tab>', 'i')
