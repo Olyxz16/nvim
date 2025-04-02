e3a9498040073afe6b72bed650a85b039218b8b6
Cédric Sazos <cedric.sazos@tutanota.com>
2025-04-02 10:00:02 +0200

    new theme rose-pine

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,24 @@
 return {
-    'f4z3r/gruvbox-material.nvim',
-    name = 'gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        require("gruvbox-material").setup({
-            contrast="soft",
-            comments={
-                italics = true,
-            },
-        })
-    end
+    {
+        'f4z3r/gruvbox-material.nvim',
+        name = 'gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("gruvbox-material").setup({
+                contrast="soft",
+                comments={
+                    italics = true,
+                },
+            })
+        end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    }
 }
